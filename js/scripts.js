2a019100cd294d7de6514d6fe2018d7836d53f20
Padma Patil <padma@email.com>
2018-08-24 14:32:25 -0700

    Got the backend logic almost working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-//frontendLogic
+//backendLogic
 function Pizza (size, toppings) {
   this.size=size;
   this.toppings=toppings;
 }
-//backendLogic
+//frontendLogic
 $(document).ready(function() {
   $("#options").submit(function(event) {
     debugger;
@@ -14,7 +14,23 @@ $(document).ready(function() {
       var chooseToppings = $(this).val();
       toppings.push(chooseToppings);
     });
+    //backendLogic
+    var toppingsPrice = (toppings.length * 0.50);
+    var sizePrice = price;
+     console.log(sizePrice);
+    if(size==="Small") {
+      var price = 8;
+    } else if(size==="Medium") {
+      var price = 10;
+    } else if(size==="Large") {
+      var price = 12
+    } else if(size==="X-Large") {
+      var price= 14;
+    }else {
+      alert("Sorry --Select-- is not an option, please use the drop down arrow to see your options.")
+    }
     var newPizza = new Pizza(size, toppings);
+    //frontendLogic
     $(".output").append("<p>" + newPizza.size + ": " + newPizza. toppings + "</p>")
   });
 });
